ee1f66e8c100ab7c2fbea25b0d6b8f5b9f26f4e5
unknown <arevir26@gmail.com>
2025-02-16 20:09:12 +0800

    Minor UI Changes

--- resources/views/market/marketlistitem.blade.php
+++ resources/views/market/marketlistitem.blade.php
@@ -1,5 +1,5 @@
 <div class="mItem">
     <a href="{{route('market.show')}}/{{$market->id}}" class="itemTitle">{{$market->market_name}}</a>
     <a href="{{route('market')}}/{{$market->id}}/update"  >Edit</a>
-    <a href="{{route('market')}}/{{$market->id}}/remove" onclick="return confirm('Are you sure?')" >Remove</a>
+    <a href="{{route('market')}}/{{$market->id}}/remove" onclick="return confirm('Are you sure you want to delete this item?')" >Remove</a>
 </div>
