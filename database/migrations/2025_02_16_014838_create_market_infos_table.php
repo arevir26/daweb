c6529646ebf8bb6e8c294f649fbfa5e0a5e76fb4
unknown <arevir26@gmail.com>
2025-02-16 11:43:55 +0800

    Added fields to market table

--- database/migrations/2025_02_16_014838_create_market_infos_table.php
+++ database/migrations/2025_02_16_014838_create_market_infos_table.php
@@ -14,7 +14,30 @@ return new class extends Migration
         Schema::create('market_infos', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
-            $table->text('market_name')->nullable();
+            $table->text('market_name');
+            $table->text('province')->nullable();
+            $table->text('municipality')->nullable();
+            $table->text('barangay')->nullable();
+            $table->text('other_address')->nullable();
+            $table->float('gps_lat')->nullable();
+            $table->float('gps_long')->nullable();
+            $table->text('market_admin')->nullable();
+            $table->integer('personnel_count')->default(null)->nullable();
+            $table->text('contact')->nullable();
+            $table->integer('market_type')->nullable();
+            $table->text('vendor_demographics')->nullable();
+            $table->text('turn_over_rate')->nullable();
+            $table->integer('management')->nullable();
+            $table->time('open_hour')->nullable();
+            $table->time('close_hour')->nullable();
+            $table->text('peak_day')->nullable();
+            $table->time('peak_hour_start')->nullable();
+            $table->time('peak_hour_end')->nullable();
+            $table->integer('foot_traffic', false, true)->nullable();
+            $table->float('area')->nullable();
+            $table->longText('remarks')->nullable();
+            $table->date('date_profiled')->nullable();
+            $table->date('date_updated')->nullable();
         });
     }
 
