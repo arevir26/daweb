17e6d655ffcf714b9306ac1f0d354fa3f79704cd
Ryan Joseph Rivera <arevir26@gmail.com>
2025-03-05 12:01:22 +0800

    Add Styles

--- resources/views/livewire/market-other-info-panel.blade.php
+++ resources/views/livewire/market-other-info-panel.blade.php
@@ -1,8 +1,28 @@
-<div>
-    <table>
+<div class="otherinfopanel" >
+    <h3>Other Market Information</h3>
+    @isset($OtherMarketInfos)
+            @foreach ($OtherMarketInfos as $otherinfo)
+                <div class="otherinfoitem">
+                    <div class="infoCat">
+                        <span>{{$otherinfo->info_type->name}}</span>
+                        <div>
+                            <a href="#" wire:click.prevent="update({{$otherinfo->id}})">Edit</a>
+                            <a href="#" wire:confirm="Do you really want to remove this item?" wire:click.prevent="remove({{$otherinfo->id}})">Remove</a>
+                        </div>
+                        
+                    </div>
+                    <div class="infoTitle">{{$otherinfo->name}}
+                    </div>
+                    <p class="infoDesc">{{$otherinfo->desc}}</p>
+                </div>
+            @endforeach
+    @endisset
+
+    <div id="OtherInfoInputPane">
         <form action="" wire:submit.prevent="add">
-        <tr>
-            <td>
+            <h5>Add other information:</h4>
+            <textarea id="marketOtherInfo" name="" id="" cols="30" rows="5" wire:model="desc"></textarea>
+            <div id="otherinputbottompanel">
                 <select name="" value="" wire:model="infotype" required>
                     <option >Select Info Type</option>
                     @isset($OtherInfoTypes)
@@ -11,39 +31,14 @@
                         @endforeach
                     @endisset
                 </select>
-            </td>
-            <td><input type="text" placeholder="Title" wire:model="title" /></td>
-            <td>
+                <input id="infotitle" type="text" placeholder="Title" wire:model="title" />
                 @if ($isUpdate)
                     <input type="submit" value="Update"  />
                 @else
                     <input type="submit" value="Add"  />
                 @endif
-                
-            </td>
-        </tr>
-        <tr>
-            <td colspan="3">
-                <textarea name="" id="" cols="30" rows="5" wire:model="desc"></textarea>
-            </td>
-        </tr>
+            </div>
         </form>
-        
-    </table>
-    @isset($OtherMarketInfos)
-        <div>
-            @foreach ($OtherMarketInfos as $otherinfo)
-                <div>
-                    <div>{{$otherinfo->name}}</div>
-                    <div>{{$otherinfo->info_type->name}}</div>
-                </div>
-                <div>
-                    <p>{{$otherinfo->desc}}</p>
-                </div>
-                <a href="#" wire:click.prevent="remove({{$otherinfo->id}})">Remove</a><br/>
-                <a href="#" wire:click.prevent="update({{$otherinfo->id}})">Edit</a>
-            @endforeach
-        </div>
-    @endisset
-    
+    </div>
+
 </div>
